a8b28ae488a79d9079b159ed2ebf8cf54527c988
casonbobo <118225257+casonbobo@users.noreply.github.com>
2024-03-11 15:08:44 -0500

    MADE 5

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -7,7 +7,8 @@ const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLID,
-  GraphQLInt
+  GraphQLInt,
+  GraphQLList
 } = require('graphql');
 
 //Setting up Lodash and the Data to pull into my Graph (2)
@@ -21,19 +22,19 @@ const tasks = [
 //projects
 const projects = [
   {
-     id: '1',
-     title: 'Advanced HTML',
-     weight: 1,
-     description: 'Welcome to the Web Stack specialization. The 3 first projects will give you all basics of the Web development: HTML, CSS and Developer tools. In this project, you will learn how to use HTML tags to structure a web page. No CSS, no styling - don’t worry, the final page will be “ugly” it’s normal, it’s not the purpose of this project. Important note: details are important! lowercase vs uppercase / wrong letter… be careful!',
+    id: '1',
+    title: 'Advanced HTML',
+    weight: 1,
+    description: 'Welcome to the Web Stack specialization. The 3 first projects will give you all basics of the Web development: HTML, CSS and Developer tools. In this project, you will learn how to use HTML tags to structure a web page. No CSS, no styling - don’t worry, the final page will be “ugly” it’s normal, it’s not the purpose of this project. Important note: details are important! lowercase vs uppercase / wrong letter… be careful!',
   },
   {
-     id: '2',
-     title: 'Bootstrap',
-     weight: 1,
-     description: 'Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development. It contains CSS and JavaScript design templates for typography, forms, buttons, navigation, and other interface components.',
+    id: '2',
+    title: 'Bootstrap',
+    weight: 1,
+    description: 'Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development. It contains CSS and JavaScript design templates for typography, forms, buttons, navigation, and other interface components.',
   },
- ];
- 
+];
+
 
 //TaskType (1)
 const TaskType = new GraphQLObjectType({
@@ -69,7 +70,6 @@ const ProjectType = new GraphQLObjectType({
   },
  });
  
-
 //RootQuery (2,3)
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
@@ -86,6 +86,18 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLString } },
       resolve(parent, args) {
         return _.find(projects, {id: args.id });
+      },
+    },
+    tasks: {
+      type: new GraphQLList(TaskType),
+      resolve(parent, args) {
+        return tasks;
+      },
+    },
+    projects: {
+      type: new GraphQLList(ProjectType),
+      resolve(parent, args) {
+        return projects;
       }
     }
   }
